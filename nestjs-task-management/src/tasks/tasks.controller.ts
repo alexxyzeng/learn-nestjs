28cae665d8e333683b05273494395aeb80d5124b
alex <xiayao.zeng@gmail.com>
2019-12-23 22:55:06 +0800

    feat: add validators

--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,5 +1,5 @@
 import { CreateTaskDto } from './dto/create-task_dto';
-import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task_model';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -10,7 +10,7 @@ export class TasksController {
 
   @Get()
   getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-    return this.taskService.getTasksWithFilters(filterDto)
+    return this.taskService.getTasksWithFilters(filterDto);
   }
 
   @Get('/:id')
@@ -19,6 +19,7 @@ export class TasksController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.taskService.createTask(createTaskDto);
   }
